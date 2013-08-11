f8d6919484281a66977c635404ace832beaa2c14
Chuck Lauer Vose <vosechu@gmail.com>
2013-08-11 16:02:15 -0700

    Patching works for existing classes or classes that have been required
    Next, add a watcher for non-existent classes and cleanup

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,10 +1,23 @@
+# Possibilities:
+#   Find a way to mixin after parse but before execution
+#   Add the method when a new string is created
+
+
+# FIXME: See if we can move this into the class so that we can have
+# multiple patches active at once
 $counts = 0
 
 class CallPatcher
-  class << self
-    def patch(target)
-      mod, func = parse(target)
-      eval("
+  attr_reader :target
+
+  def initialize(target)
+    @target = target
+  end
+
+  def patch
+    mod, func, instance_or_class = parse
+    if mod.is_a?(Class) && func.is_a?(Symbol)
+      patch = "
         class ::#{mod}
           def #{func}_with_counter
             $counts += 1
@@ -13,16 +26,42 @@ class CallPatcher
           alias_method :#{func}_without_counter, :#{func}
           alias_method :#{func}, :#{func}_with_counter
         end
-      ")
+      "
+      eval(patch)
+    else
+      # FIXME: Create a watcher that we can target for when methods are
+      # added after the fact.
+      puts "hi mom"
+    end
+  end
+
+  def parse
+    mod, func = target.split(/[\.#]/)
+
+    begin
+      # Create Classes and Symbols to indicate happiness
+      mod = Kernel.const_get(mod)
+      func = func.to_sym
+      if mod.respond_to?(func)
+        instance_or_class = :class
+      elsif mod.new.respond_to?(func)
+        instance_or_class = :instance
+      end
+    rescue NameError => e
+      # Use the string as parsed from above
     end
 
-    def parse(target)
-      return target.split(/[\.#]/)
-    end
+    return [mod, func, instance_or_class]
   end
 end
 
+# FIXME: Can we clean this up a little bit? I'd like to not have the
+# `require` kick off the whole process.
 target = ENV.fetch('COUNT_CALLS_TO') { false }
 if target
-  CallPatcher.patch(target)
+  CallPatcher.new(target).patch
+
+  at_exit do
+    puts "#{target} called #{$counts} times"
+  end
 end
--- spec/solution_spec.rb
+++ spec/solution_spec.rb
@@ -2,35 +2,17 @@ require 'rspec'
 
 describe "Counting calls" do
 
-  before(:each) do
-    pending
-  end
-
   context "String#size" do
-    before(:each) do
-      ENV['COUNT_CALLS_TO'] = 'String#size'
-      require './lib/solution'
-    end
-
     it "should output the number of calls" do
-      out = capture_stdout do
-        (1..100).each{|i| i.to_s.size if i.odd? }
-      end
-      out.should eq 'String#size called 50 times'
+      out = `COUNT_CALLS_TO='String#size' ruby -r ./lib/solution.rb -e '(1..100).each{|i| i.to_s.size if i.odd? }'`
+      out.should eq "String#size called 50 times\n"
     end
   end
 
   context "B#foo" do
-    before(:each) do
-      ENV['COUNT_CALLS_TO'] = 'B#foo'
-      require './lib/solution'
-    end
-
     it "should output the number of calls" do
-      out = capture_stdout do
-        module A; def foo; end; end; class B; include A; end; 10.times{B.new.foo}
-      end
-      out.should eq 'B#foo called 10 times'
+      out = `COUNT_CALLS_TO='B#foo' ruby -r ./lib/solution.rb -e 'module A; def foo; end; end; class B; include A; end; 10.times{B.new.foo}'`
+      out.should eq "B#foo called 10 times"
     end
   end
 
@@ -39,31 +21,53 @@ end
 describe "Module/function splitting" do
 
   before(:each) do
-    require './lib/solution'
+    # Use load in this case because we want to repatch things each time
+    # FIXME: Is there a way to make rspec force a new binding each time?
+    load './lib/solution.rb'
   end
 
   it "should split functions" do
-    CallPatcher.parse('Array#map!').should eq(['Array', 'map!'])
-    CallPatcher.parse('ActiveRecord::Base#find').should eq(['ActiveRecord::Base', 'find'])
-    CallPatcher.parse('Base64.encode64').should eq(['Base64', 'encode64'])
-    mod, func = CallPatcher.parse('String#size')
-    mod.should eq("String")
-    func.should eq("size")
+    CallPatcher.new('String#size').parse.should eq([String, :size, :instance])
+  end
+
+  it "should not return classes for as-yet undefined methods" do
+    CallPatcher.new('ActiveRecord::Base#find').parse.should eq(["ActiveRecord::Base", "find", nil])
+    CallPatcher.new('B#foo').parse.should eq(["B", 'foo', nil])
+  end
+
+  it "should understand whether a required method is instance or class" do
+    CallPatcher.new('Array#map!').parse.should eq([Array, :map!, :instance])
+    CallPatcher.new('String#try_convert').parse.should eq([String, :try_convert, :class])
+  end
+
+  it "should return classes if they're required" do
+    CallPatcher.new('Base64.encode64').parse.should eq(["Base64", "encode64", nil])
+    require 'base64'
+    CallPatcher.new('Base64.encode64').parse.should eq([Base64, :encode64, :class])
+  end
+
+  it "should split into multiple vars" do
+    mod, func, instance_or_class = CallPatcher.new('String#size').parse
+    mod.should eq(String)
+    func.should eq(:size)
+    instance_or_class.should eq(:instance)
   end
 end
 
 describe "Counting simple things" do
   before(:each) do
-    require './lib/solution'
+    ENV['COUNT_CALLS_TO'] = "String#upcase"
+    load './lib/solution.rb'
   end
 
-  it "should start at 0" do
-    $counts.should eq(0)
-  end
+  # # FIXME: Find a way to unmake these changes each time we run
+  # it "should undefine the new method each time" do
+  #   "hello".should_not respond_to(:upcase_without_counter)
+  # end
 
   it "should count to 1" do
-    CallPatcher.patch('String#size')
-    "hello".size
+    $counts.should eq(0)
+    "hello".upcase
     $counts.should eq(1)
   end
 end
